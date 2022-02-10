becd857cb48ce755a1c06e934c558029e04412b0
Dhia Nabli <dhianabli@outlook.com>
2022-02-10 15:15:35 +0100

    connect sqlite db

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
+import uvicorn
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
+
+
+@app.get("/")
+def hello_api():
+    return {"detail":"Hello World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8001)
